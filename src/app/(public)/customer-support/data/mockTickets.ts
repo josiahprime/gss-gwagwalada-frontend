c1b39d0f07b71168b417b0f99e3e0c21715dfd52
josiah prime <choujiakimichi02@gmail.com>
2026-06-16 01:47:01 +0100

    changes were made 4

--- src/app/(public)/customer-support/data/mockTickets.ts
+++ src/app/(public)/customer-support/data/mockTickets.ts
@@ -1,29 +1,29 @@
 import type { Ticket } from "store/ticket/ticketTypes";
 
 
-export const mockTickets: Ticket[] = [
-  {
-    id: "TCK-1001",
-    subject: "Delayed delivery of maize seeds",
-    category: "delivery",
-    priority: "high",
-    status: "open",
-    createdAt: new Date("2026-01-12T10:30:00Z"),
-    updatedAt: new Date("2026-01-13T08:15:00Z"),
-    customerId: "CUST-1",
-    customerName: "John Ade",
-    customerEmail: "john@example.com",
-    messages: [
-      {
-        id: "MSG-1",
-        sender: "customer",
-        senderName: "John Ade",
-        content: "My delivery was supposed to arrive last week but I haven’t received anything.",
-        timestamp: new Date("2026-01-12T10:30:00Z"),
-      },
-    ],
-  },
-];
+// export const mockTickets: Ticket[] = [
+//   {
+//     id: "TCK-1001",
+//     subject: "Delayed delivery of maize seeds",
+//     category: "delivery",
+//     priority: "high",
+//     status: "open",
+//     createdAt: new Date("2026-01-12T10:30:00Z"),
+//     updatedAt: new Date("2026-01-13T08:15:00Z"),
+//     customerId: "CUST-1",
+//     customerName: "John Ade",
+//     customerEmail: "john@example.com",
+//     messages: [
+//       {
+//         id: "MSG-1",
+//         sender: "customer",
+//         senderName: "John Ade",
+//         content: "My delivery was supposed to arrive last week but I haven’t received anything.",
+//         timestamp: new Date("2026-01-12T10:30:00Z"),
+//       },
+//     ],
+//   },
+// ];
 
 export interface Message {
   id: string;
